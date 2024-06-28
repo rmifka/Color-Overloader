6d227a95d115ba615234f4bd0c6994ee2998d49c
Mifka Rene <rene.mifka@gmail.com>
2024-06-28 22:19:04 +0200

    Added meta tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,12 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Color-Overloader';
+
+  constructor(private meta: Meta) {
+    this.meta.addTag({ property: 'og:title', content: 'Color Overloader' });
+    this.meta.addTag({ property: 'og:description', content: `Renschi's Color Overloader` });
+    this.meta.addTag({ property: 'og:url', content: 'https://rmifka.github.io/Color-Overloader/' });
+    this.meta.addTag({ property: 'og:image', content: 'https://avatars.githubusercontent.com/u/73305789?v=4' });
+    this.meta.addTag({ name: 'theme-color', content: '#43B581', 'data-react-helmet': 'true' });
+  }
 }
